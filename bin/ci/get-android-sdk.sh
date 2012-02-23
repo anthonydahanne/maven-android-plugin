f3eed2bba344d3f4c6e814395cff468f1c64a0e5
Anthony Dahanne <anthony.dahanne@gmail.com>
2012-02-23 17:21:46 -0500

    trying out travis before_install

---
+++ bin/ci/get-android-sdk.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget http://dl.google.com/android/android-sdk_r16-linux.tgz
+ls
+tar xvzf android-sdk_r16-linux.tgz
+ls android-sdk-linux
+android-sdk-linux/tool/android update sdk --no-ui -t platform –force
+ls android-sdk-linux/platforms
