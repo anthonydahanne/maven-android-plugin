d64d0814910e3db8b588c243cd6d9e29a1ffcff7
Anthony Dahanne <anthony.dahanne@gmail.com>
2012-02-23 17:35:33 -0500

    trying out travis before_install

--- bin/ci/get-android-sdk.sh
+++ bin/ci/get-android-sdk.sh
@@ -6,6 +6,6 @@ echo "unzipping android-sdk_r16-linux.tgz"
 tar xvzf android-sdk_r16-linux.tgz
 #ls android-sdk-linux
 echo "getting some Android platforms"
-android-sdk-linux/tools/android update sdk --no-ui -t platform –force
+android-sdk-linux/tools/android update sdk --no-ui -t platform
 echo "here is the list of the downloaded platforms : "
 ls android-sdk-linux/platforms
